8fa5b718ed42b90b0d28f60d04ce9aa93a7e96a6
Shobhnik13 <shobhnikw@gmail.com>
2023-06-19 05:37:22 +0530

    home page

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import Header from '../../components/Header/Header'
-
+import Featured from '../../components/Featured/Featured'
+import '../HomePage/HomePage.css'
+import PropertyList from '../../components/PropertyList/PropertyList'
 const HomePage = () => {
   return (
     <div>
         <Navbar/>
         <Header/>
+        <div className="homeContainer">
+          <Featured/>
+          <h1 className="homeTitle font-bold text-xl">Browse by property type</h1>
+          <PropertyList/>
+        </div>
     </div>
   )
 }
