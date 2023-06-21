d7c05d1d1ca8202600e6153bf530d5bd6a631d9f
Shobhnik13 <shobhnikw@gmail.com>
2023-06-21 23:45:39 +0530

    chnges

--- frontend/src/components/EmailList/MailList.jsx
+++ frontend/src/components/EmailList/MailList.jsx
@@ -1,7 +1,7 @@
 import '../EmailList/MailList.css'
 const MailList = () => {
     return (
-      <div className="mail bg-gradient-to-l from-blue-700 to-blue-400">
+      <div className="mail bg-gradient-to-l from-blue-700 to-blue-400 ">
         <h1 className="mailTitle text-3xl font-semibold text-blue-100">Stay comfortable, stay easy!</h1>
         <span className="mailDesc text-lg ">Sign up and we'll send the best deals to you</span>
         <div className="mailInputContainer">
